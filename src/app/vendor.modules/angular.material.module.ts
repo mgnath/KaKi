55e77f26af3773e2d49da893e1fe19ffc484d219
Gopinath <gopinath.manchineella@gmail.com>
2018-04-21 15:02:44 -0400

    kaki init

---
+++ src/app/vendor.modules/angular.material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule,
+        MatSidenavModule, MatIconModule, MatFormFieldModule,
+        MatInputModule, MatSelectModule, MatTableModule, MatCardModule,
+        MatProgressSpinnerModule, MatSliderModule, MatRadioModule} from '@angular/material';
+
+@NgModule({
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule,
+            MatSidenavModule, MatIconModule, MatFormFieldModule,
+            MatInputModule, MatSelectModule, MatTableModule, MatCardModule,
+            MatProgressSpinnerModule, MatSliderModule, MatRadioModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule,
+            MatSidenavModule, MatIconModule, MatFormFieldModule,
+            MatInputModule, MatSelectModule, MatTableModule, MatCardModule,
+            MatProgressSpinnerModule, MatSliderModule, MatRadioModule],
+  declarations: []
+})
+export class AngularMaterialModule { }
