c5d78f899575cad6231442c201b51da398059578
Gopinath <gopinath.manchineella@gmail.com>
2018-04-27 18:42:27 -0400

    switch to scss

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environments/environment.prod';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
+  version = '0.0.1';
   constructor() { }
 
   ngOnInit() {
+    this.version = environment.version;
   }
 
 }
